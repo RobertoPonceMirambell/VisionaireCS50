82c6ec149a1d17c895de01ceb17fb68b00574fee
Roberto Ponce <robertoponce@college.harvard.edu>
2018-11-29 21:43:17 -0500

    Set up pages

---
+++ VisionaireCS50/VisionaireCS50/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionaireCS50
+//
+//  Created by Roberto Ponce on 11/29/18.
+//  Copyright © 2018 Roberto Ponce. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
